0da5a2aa24b2e16d3a0251ce91be4207f5dc3cab
Ngoguey42 <ngoguey@student.42.fr>
2015-04-27 13:38:09 +0200

    having fun with opengl

--- OpenGL/main.cpp
+++ OpenGL/main.cpp
@@ -13,49 +13,109 @@ static void key_callback(GLFWwindow* window, int key, int, int action, int)
 		glfwSetWindowShouldClose(window, GL_TRUE);
 }
 
-int main(void)
+void draw_triangle(GLFWwindow* window)
+{
+	float ratio;
+	int width, height; //useless?
+
+	glfwGetFramebufferSize(window, &width, &height); //useless?
+	ratio = width / static_cast<float>(height); //useless?
+	glViewport(0, 0, width, height); //useless?
+	glClear(GL_COLOR_BUFFER_BIT);
+	glMatrixMode(GL_PROJECTION); //useless?
+	glLoadIdentity(); //useless?
+	glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f); //useless?
+	glMatrixMode(GL_MODELVIEW); //useless?
+	glLoadIdentity();
+	glRotatef(static_cast<float>(glfwGetTime()) * 50.f, 0.f, 0.f, 1.f);
+	glBegin(GL_TRIANGLES);
+	glColor3f(1.f, 0.f, 0.f);
+	glVertex3f(-0.6f, -0.4f, 0.f);
+	glColor3f(0.f, 1.f, 0.f);
+	glVertex3f(0.6f, -0.4f, 0.f);
+	glColor3f(0.f, 0.f, 1.f);
+	glVertex3f(0.f, 0.6f, 0.f);
+
+
+	glEnd();
+	glfwSwapBuffers(window);
+	glfwPollEvents();
+	return ;
+}
+
+
+void draw_grid(GLFWwindow* window)
+{
+	float ratio;
+	int width, height; //useless?
+
+	glfwGetFramebufferSize(window, &width, &height); //useless?
+	ratio = width / static_cast<float>(height); //useless?
+	glViewport(0, 0, width, height); //useless?
+	glClear(GL_COLOR_BUFFER_BIT);
+	glMatrixMode(GL_PROJECTION); //useless?
+	glLoadIdentity(); //useless?
+	glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f); //useless?
+	glMatrixMode(GL_MODELVIEW); //useless?
+	glLoadIdentity();
+	// glRotatef(static_cast<float>(glfwGetTime()) * 50.f, 0.f, 0.f, 1.f);
+	glBegin(GL_LINES);
+
+	glColor3f(1.f, 0.f, 0.f);
+	for (float y = -0.9f; y < 0.95f; y += 0.1)
+	{
+		glVertex3f(-0.9f, y, 0.f);
+		glVertex3f(0.9f, y, 0.f);
+	}
+	for (float x = -0.9f; x < 0.95f; x += 0.1)
+	{
+		glVertex3f(x, -0.9f, 0.f);
+		glVertex3f(x, 0.9f, 0.f);
+	}
+	/*
+	glColor3f(0.f, 1.f, 0.f);
+	glVertex3f(0.6f, -0.4f, 0.f);
+	glColor3f(0.f, 0.f, 1.f);
+	glVertex3f(0.f, 0.6f, 0.f);
+	*/
+
+	glEnd();
+	glfwSwapBuffers(window);
+	glfwPollEvents();
+	return ;
+}
+
+
+void init_window(int x, int y)
 {
 	GLFWwindow*		window;
 
-	glfwSetErrorCallback(error_callback);
-	if (!glfwInit())
-		exit(EXIT_FAILURE);
-	window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
+	window = glfwCreateWindow(x, y, "Simple example", NULL, NULL);
 	if (!window)
 	{
 		glfwTerminate();
 		exit(EXIT_FAILURE);
 	}
-	glfwMakeContextCurrent(window);
-	glfwSwapInterval(1);
+	glfwMakeContextCurrent(window); //useless?
+	glfwSwapInterval(1); //useless?
 	glfwSetKeyCallback(window, key_callback);
 	while (!glfwWindowShouldClose(window))
 	{
-		float ratio;
-		int width, height;
-
-		glfwGetFramebufferSize(window, &width, &height);
-		ratio = width / static_cast<float>(height);
-		glViewport(0, 0, width, height);
-		glClear(GL_COLOR_BUFFER_BIT);
-		glMatrixMode(GL_PROJECTION);
-		glLoadIdentity();
-		glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
-		glMatrixMode(GL_MODELVIEW);
-		glLoadIdentity();
-		glRotatef(static_cast<float>(glfwGetTime()) * 50.f, 0.f, 0.f, 1.f);
-		glBegin(GL_TRIANGLES);
-		glColor3f(1.f, 0.f, 0.f);
-		glVertex3f(-0.6f, -0.4f, 0.f);
-		glColor3f(0.f, 1.f, 0.f);
-		glVertex3f(0.6f, -0.4f, 0.f);
-		glColor3f(0.f, 0.f, 1.f);
-		glVertex3f(0.f, 0.6f, 0.f);
-		glEnd();
-		glfwSwapBuffers(window);
-		glfwPollEvents();
+		// draw_triangle(window);
+		draw_grid(window);
 	}
-	glfwDestroyWindow(window);
+	glfwDestroyWindow(window);	
+	return ;
+}
+
+
+int main(void)
+{
+	glfwSetErrorCallback(error_callback);
+	if (!glfwInit())
+		exit(EXIT_FAILURE);
+	init_window(500, 500);
+	init_window(200, 200);
 	glfwTerminate();	
 	return (0);
 }
