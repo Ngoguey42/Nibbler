02237054e28c450c26bea0a025149ebbbf67f86a
jaguillo <juloo.dsi@gmail.com>
2015-05-20 17:04:16 +0200

    Load sprites/font only once

--- SfmlUI/srcs/init.cpp
+++ SfmlUI/srcs/init.cpp
@@ -6,7 +6,7 @@
 /*   By: jaguillo <jaguillo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/05/12 17:48:30 by jaguillo          #+#    #+#             */
-/*   Updated: 2015/05/12 17:49:05 by jaguillo         ###   ########.fr       */
+/*   Updated: 2015/05/20 17:02:16 by jaguillo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,29 @@ extern "C"
 
 IUI				*INIT_FUNCTION(std::pair<int, int> gameSize)
 {
-	return (new SfmlUI(gameSize));
+	static sf::Texture	*sprites = NULL;
+	static sf::Font		*font = NULL;
+
+	if (sprites == NULL)
+	{
+		sprites = new sf::Texture();
+		if (!sprites->loadFromFile(SPRITES_LOCATION))
+		{
+			delete sprites;
+			throw std::runtime_error("Cannot load sprites");
+		}
+		sprites->setSmooth(true);
+	}
+	if (font == NULL)
+	{
+		font = new sf::Font();
+		if (!font->loadFromFile(FONT_LOCATION))
+		{
+			delete font;
+			throw std::runtime_error("Cannot load font");
+		}
+	}
+	return (new SfmlUI(gameSize, *sprites, *font));
 }
 
 }
